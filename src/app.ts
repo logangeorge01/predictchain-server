af9f8eeac30321bec3a7b789bab2066b8e0d263f
BryanWhitehurst <bryan@Bryans-MacBook-Air.local>
2022-02-23 13:44:51 -0600

    added /event endpoint to create document in mongo

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,47 @@
 import express, { Application, Request, Response } from 'express';
+var MongoClient = require('mongodb').MongoClient;
+
+const bodyParser = require("body-parser"); //middleware required to handle POST requests (npm install --save body-parser)
+
+var url = "mongodb://localhost:27017/"; //address to local mongoDB
 
 const app: Application = express();
+const router = express.Router();
 const port: number = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+//code to tell app to use bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/**
+ * /event endpoint
+ * expecting 3 body params
+ *  wallet_id: string
+ *  event_name: string
+ *  event_desc: string 
+ */
+app.post("/event", (req: Request, res: Response) => {
+    const walletId = req.body.wallet_id;
+    const eventName = req.body.event_name;
+    const eventDesc = req.body.event_desc;
+    
+    //creates document in local mongoDB in 'events' collection
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("predict_chain");
+        var myobj = { wallet_id: walletId, event_name: eventName, event_desc: eventDesc};
+        dbo.collection("event").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      });
+
+    res.send({
+        'wallet_id': walletId,
+        'event_name': eventName,
+        'event_desc': eventDesc
+      });
 });
 
 app.listen(port, () => {
