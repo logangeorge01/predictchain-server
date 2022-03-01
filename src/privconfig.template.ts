d5341a16141734a7c4fe66313e871c3dd8964423
BryanWhitehurst <bryan@Bryans-MacBook-Air.local>
2022-03-01 12:45:35 -0600

    added my solana wallet address

--- src/privconfig.template.ts
+++ src/privconfig.template.ts
@@ -1,5 +1,6 @@
 // Private configuraiton template for PredictChain Server
 
 export const PrivConfig = {
-    admins: ['FDwi8C9fWuaNn7dvNkWWAxq8k5HWTkjBVYUGR5dE831W'], // Place admins here
+    admins: ['FDwi8C9fWuaNn7dvNkWWAxq8k5HWTkjBVYUGR5dE831W', 
+             'Hnwiog7ZZCaA5tFEACWcFkq3mgNAELwK6HYFLYhajSVy'], // Place admins here
 };
---
+++ src/secret.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAPnsZxE5wKCYC0QM-0PAqQUAADl49xcc8",
+    authDomain: "predictchain.firebaseapp.com",
+    projectId: "predictchain",
+    storageBucket: "predictchain.appspot.com",
+    messagingSenderId: "572204008537",
+    appId: "1:572204008537:web:bdbb1ef3616f0dfca89d48"
+    };
