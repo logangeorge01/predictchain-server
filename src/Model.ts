8847ff266b3a4c3974017ecc9e6d740a865949f7
BryanWhitehurst <bryan@Bryans-MacBook-Air.local>
2022-03-01 13:38:47 -0600

    added ability to create new document in db

--- src/Model.ts
+++ src/Model.ts
@@ -8,11 +8,23 @@
  */
 
 import { Db } from 'mongodb';
-
+import { MongoClient } from 'mongodb';
 export class Model {
-    private db: Db;
-
-    constructor(db: Db) {
-        this.db = db;
+    public uri = "mongodb://localhost:27017/";
+    public client : MongoClient;
+    
+    constructor() {
+        this.client = new MongoClient(this.uri);
     }
+    
+    async openConnection(){
+        await this.client.connect();
+    }
+
+    //add document
+    async createListing(newListing){
+        const result = await this.client.db("predict_chain").collection("events").insertOne(newListing);
+        console.log(`New listing created with the following id: ${result.insertedId}`);
+    }
+    
 }
